75e4b567229f2a8854f84bd638d0b4352b9187ef
Thirupathi Peraboina <thirupathiperaboina@gmail.com>
2020-07-22 15:17:00 +0530

    added code to procces

--- proccess/app.py
+++ proccess/app.py
@@ -1,7 +1,13 @@
-import time
-while True:
-  localtime = time.localtime()
-  result = time.strftime("%I:%M:%S %p", localtime)
-  print(result)
-  print("Process 1")
-  time.sleep(50)
+names = ['David', 'Peter', 'Michael', 'John', 'Bob']
+for i in range (len (names)):
+    print("{}.{}".format(i + 1, names[i]))
+
+
+
+# import time
+# while True:
+#   localtime = time.localtime()
+#   result = time.strftime("%I:%M:%S %p", localtime)
+#   print(result)
+#   print("Process 1")
+#   time.sleep(50)
