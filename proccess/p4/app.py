59141589ed29735e63a4c0bc18897001ddefcce4
Thirupathi Peraboina <thirupathiperaboina@gmail.com>
2020-07-22 15:47:30 +0530

    added code to procces

---
+++ proccess/p4/app.py
@@ -0,0 +1,19 @@
+import time
+
+names = ['Process 4']
+while True:
+    for i in range (len (names)):
+        localtime = time.localtime()
+        result = time.strftime("%I:%M:%S %p", localtime)
+        print(result)
+        print("{}.{}".format(i + 1, names[i]))
+    continue
+
+
+# import time
+# while True:
+#   localtime = time.localtime()
+#   result = time.strftime("%I:%M:%S %p", localtime)
+#   print(result)
+#   print("Process 1")
+#   time.sleep(50)
